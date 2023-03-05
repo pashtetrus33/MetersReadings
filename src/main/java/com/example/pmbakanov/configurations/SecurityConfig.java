5e85f2a737e65db244151e11ec9097cddc9877d7
pashtetrus33 <testerrus33@gmail.com>
2023-03-06 00:55:51 +0300

    050323

--- src/main/java/com/example/pmbakanov/configurations/SecurityConfig.java
+++ src/main/java/com/example/pmbakanov/configurations/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/images/**", "/static/**","/registration")
+                .antMatchers("/", "/images/**", "/static/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
--- src/main/java/com/example/pmbakanov/controllers/AdminController.java
+++ src/main/java/com/example/pmbakanov/controllers/AdminController.java
@@ -45,6 +45,20 @@ public class AdminController {
         return "alluserrequests";
     }
 
+    @GetMapping("/registration")
+    public String registration(Principal principal, Model model) {
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String createUser(User user, Model model) {
+        if (!userService.createUser(user)) {
+            model.addAttribute("errorMessage", "Пользователь с именем: " + user.getLogin() + " уже существует");
+            return "registration";
+        }
+        return "redirect:/profile";
+    }
 
     @GetMapping("/admin/user/edit/{user}")
     public String userEdit(@PathVariable("user") User user, Model model, Principal principal) {
