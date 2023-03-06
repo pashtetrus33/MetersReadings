6dc299ea667f7fae0a47e967d88bb69bd2c28620
pashtetrus33 <testerrus33@gmail.com>
2023-03-06 20:50:08 +0300

    050323

--- src/main/java/com/example/pmbakanov/services/UserService.java
+++ src/main/java/com/example/pmbakanov/services/UserService.java
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @RequiredArgsConstructor
 public class UserService {
+    @Autowired
     private MailSender mailSender;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
