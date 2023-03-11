199c2059e9e1c595a4f7111f46e981b87a179069
pashtetrus33 <testerrus33@gmail.com>
2023-03-12 02:27:51 +0300

    110323

--- src/main/java/com/example/pmbakanov/configurations/SecurityConfig.java
+++ src/main/java/com/example/pmbakanov/configurations/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/images/**", "/static/**", "/activate/*", "/registration", "/reset/**" , "/newpassword/**")
+                .antMatchers("/", "/images/**", "/static/**", "/activate/*", "/registration", "/reset/**" , "/newpassword")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
