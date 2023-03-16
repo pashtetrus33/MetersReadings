89ee81f29929ebd43466e617ff2c8c9f730db090
pashtetrus33 <testerrus33@gmail.com>
2023-03-17 00:05:39 +0300

    110323

--- src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
+++ src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
@@ -16,16 +16,9 @@ public class ScheduleTasksService {
 
     @Scheduled(cron = "${interval-in-cron}")
     public void periodStartMailNotification() throws InterruptedException {
-//        for (User person: userRepository.findAll()) {
-//            mailSender.sendMail(person.getEmail(), "Начало периода подачи показаний счетчиков воды",
-//                    "Добрый день," + person.getName() + "\n" + "Пожалуйста, передайте показания счетчиков воды до 24 числа текущего месяца.");
-//        }
-        mailSender.sendMail("pashtet_rus@mail.ru", "Начало периода подачи показаний счетчиков воды",
-                "Добрый день, пожалуйста, передайте показания счетчиков воды до 24 числа текущего месяца.");
+        for (User person : userRepository.findAll()) {
+            mailSender.sendMail(person.getEmail(), "Начало периода подачи показаний счетчиков воды",
+                    "Добрый день, " + person.getName() + "\n" + "Пожалуйста, передайте показания счетчиков воды до 24 числа текущего месяца.");
+        }
     }
-
-//    @Scheduled(cron = "${interval-in-cron2}")
-//    public void showTimeAndDate() throws InterruptedException {
-//        LocalDateTime localDateTime = LocalDateTime.now();
-//    }
 }
