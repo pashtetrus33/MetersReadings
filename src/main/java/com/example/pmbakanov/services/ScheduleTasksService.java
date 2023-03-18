513540295fa6982b936670b0fbb9270875c72c41
pashtetrus33 <testerrus33@gmail.com>
2023-03-18 23:41:58 +0300

    110323

--- src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
+++ src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
@@ -37,7 +37,7 @@ public class ScheduleTasksService {
             boolean check = person.areRecords();
             if ((!person.areRecords()) || (person.getLastRecord().getDateOfCreated().getMonth() != LocalDateTime.now().getMonth())) {
                 mailSender.sendMail("pashtet_rus@mail.ru", "Окончание периода подачи показаний счетчиков воды",
-                        "Добрый день, " + person.getName() + "\n" +
+                        "Добрый день, " + person.getName() + ".\n" +
                                 "Пожалуйста, передайте показания счетчиков воды.");
             }
         }
