f8c36e500ac82289130f347bf65ed693e61147db
pashtetrus33 <testerrus33@gmail.com>
2023-03-19 15:36:25 +0300

    110323

--- src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
+++ src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
@@ -27,12 +27,6 @@ public class ScheduleTasksService {
 
     @Scheduled(cron = "${interval-in-cron2}")
     public void periodAlmostFinishedMailNotification() throws InterruptedException {
-//        for (User person : userRepository.findAll()) {
-//            mailSender.sendMail("pashtet_rus@mail.ru", "Начало периода подачи показаний счетчиков воды",
-//                    "Добрый день, " + person.getName() + ".\n" + "Пожалуйста, передайте показания счетчиков воды до 24 числа текущего месяца.");
-//        }
-
-
         for (User person : userRepository.findAll()) {
             boolean check = person.areRecords();
             if ((!person.areRecords()) || (person.getLastRecord().getDateOfCreated().getMonth() != LocalDateTime.now().getMonth())) {
