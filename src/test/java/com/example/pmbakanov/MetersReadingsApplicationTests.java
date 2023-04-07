fc69714179a2e901f61852c5fef840fc7e31240b
pashtetrus33 <testerrus33@gmail.com>
2023-04-07 05:07:04 +0300

    Comment tests

--- src/test/java/com/example/pmbakanov/MetersReadingsApplicationTests.java
+++ src/test/java/com/example/pmbakanov/MetersReadingsApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.pmbakanov;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MetersReadingsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
