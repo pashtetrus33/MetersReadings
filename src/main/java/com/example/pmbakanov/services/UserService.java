e978780de6f40271bbbe3281b333b95e962a28be
pashtetrus33 <testerrus33@gmail.com>
2023-04-12 03:49:45 +0300

    DEPLOY_WEBSITE = "https://chile.onrender.com"

--- src/main/java/com/example/pmbakanov/services/UserService.java
+++ src/main/java/com/example/pmbakanov/services/UserService.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @RequiredArgsConstructor
 public class UserService {
-    private static final String DEPLOY_WEBSITE = "https://meters.onrender.com";
+    private static final String DEPLOY_WEBSITE = "https://chile.onrender.com";
     private final MailSender mailSender;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
