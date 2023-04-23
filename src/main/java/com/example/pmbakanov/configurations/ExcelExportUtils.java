ac0d2b7925a1728966181d81c5b26523bd0f0700
pashtetrus33 <testerrus33@gmail.com>
2023-04-23 22:11:27 +0300

    integer to float

--- src/main/java/com/example/pmbakanov/configurations/ExcelExportUtils.java
+++ src/main/java/com/example/pmbakanov/configurations/ExcelExportUtils.java
@@ -137,12 +137,12 @@ public class ExcelExportUtils {
             createCell(row, columnCount++, record.getDateOfCreatedString(), style);
             createCell(row, columnCount++, record.getUser().getName(), style);
             createCell(row, columnCount++, record.getUser().getAddress(), style);
-            createCell(row, columnCount++, record.getKitchenCold(), style);
-            createCell(row, columnCount++, record.getKitchenHot(), style);
-            createCell(row, columnCount++, record.getToiletCold(), style);
-            createCell(row, columnCount++, record.getToiletHot(), style);
-            createCell(row, columnCount++, record.getNeighborCold(), style);
-            createCell(row, columnCount, record.getNeighborHot(), style);
+            createCell(row, columnCount++, String.format("%.3f",record.getKitchenCold()), style);
+            createCell(row, columnCount++, String.format("%.3f",record.getKitchenHot()), style);
+            createCell(row, columnCount++, String.format("%.3f",record.getToiletCold()), style);
+            createCell(row, columnCount++, String.format("%.3f",record.getToiletHot()), style);
+            createCell(row, columnCount++, String.format("%.3f",record.getNeighborCold()), style);
+            createCell(row, columnCount, String.format("%.3f",record.getNeighborHot()), style);
         }
 
         for (ElectricityRecord electricityRecord : electricityRecordList) {
