e051072f9385ac4907eb81df9a517d8db24e767e
pashtetrus33 <testerrus33@gmail.com>
2023-05-20 15:37:37 +0300

    url for service changed

--- src/main/java/com/example/pmbakanov/services/UserService.java
+++ src/main/java/com/example/pmbakanov/services/UserService.java
@@ -17,8 +17,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @RequiredArgsConstructor
 public class UserService {
-    //static final String DEPLOY_WEBSITE = "https://chile.onrender.com";
-    static final String DEPLOY_WEBSITE = "http://185.87.51.45";
+    static final String DEPLOY_WEBSITE = "http://chilemeters.ru";
 
     private final MailSender mailSender;
     private final UserRepository userRepository;
