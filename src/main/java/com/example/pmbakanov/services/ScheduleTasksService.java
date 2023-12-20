e27216b64ce8955b8279a8193bc689923a7d3dfe
pashtetrus33 <testerrus33@gmail.com>
2023-12-20 04:13:10 +0300

    fix

--- src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
+++ src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
@@ -9,16 +9,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
-
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.List;
-
 import static com.example.pmbakanov.services.UserService.DEPLOY_WEBSITE;
-import static com.example.pmbakanov.services.UserService.DEPLOY_WEBSITE_REDIRECT;
 
 @Service
 @RequiredArgsConstructor
