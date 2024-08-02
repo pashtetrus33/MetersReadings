deb046c92cc034c329087952768a7734502deaee
Pavel Bakanov <pashtetrus@gmail.com>
2024-08-02 11:40:20 -0400

    fix mail admin and cron

--- src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
+++ src/main/java/com/example/pmbakanov/services/ScheduleTasksService.java
@@ -38,6 +38,7 @@ public class ScheduleTasksService {
      */
     @Scheduled(cron = "${interval-in-cron}")
     public void periodStartMailNotification() {
+        log.info("periodStartMailNotification");
         List<User> activeUsersList = userRepository.findAllByActiveIsTrue();
         for (User person : activeUsersList) {
             mailSender.sendMail(person.getEmail(), "Начало периода подачи показаний счетчиков воды",
@@ -46,12 +47,21 @@ public class ScheduleTasksService {
         }
     }
 
+    @Scheduled(cron = "${interval-in-cron3}")
+    public void testMailNotification() {
+        log.info("periodStartMailNotification");
+            mailSender.sendMail("pashtetrus@gmail.com", "Начало периода подачи показаний счетчиков воды",
+                    "Добрый день, " + ".\n" + "Пожалуйста, передайте показания счетчиков воды до 24 числа текущего месяца."
+                            + ".\n" + DEPLOY_WEBSITE);
+    }
+
     /**
      * Уведомление о завершении периода подачи показаний счетчиков воды.
      */
     @Scheduled(cron = "${interval-in-cron2}")
     @Transactional
     public void periodAlmostFinishedMailNotification() {
+        log.info("periodAlmostFinishedMailNotification");
         List<User> activeUsersList = userRepository.findAllByActiveIsTrue();
         for (User person : activeUsersList) {
             if ((!person.areMeterReadings()) || (person.getLastMeterReading().getDateOfCreated().getMonth() != LocalDateTime.now().getMonth())) {
@@ -59,27 +69,6 @@ public class ScheduleTasksService {
                         "Добрый день, " + person.getName() + ".\n" +
                                 "Пожалуйста, передайте показания счетчиков воды." + ".\n" + DEPLOY_WEBSITE);
             }
-        }
-    }
-
-    // Метод периодического пинга бывшего расположения системы для корректной работы (ONRENDER.COM)
-    @Value("${pingtask.url}")
-    private String url;
-
-    /**
-     * Периодический пинг для корректной работы системы.
-     */
-    @Scheduled(fixedRateString = "${pingtask.period}")
-    public void pingMe() {
-        try {
-            URL url = new URL(getUrl());
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-            log.info("Ping {}, OK: response code {}", url.getHost(), connection.getResponseCode());
-            connection.disconnect();
-        } catch (IOException e) {
-            log.error("Ping FAILED");
-            e.printStackTrace();
         }
     }
 }
